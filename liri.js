c7a2d1ccf1fc74bb997747ed3e38c651271d190b
nate pearson <nate913@gmail.com>
2019-04-27 18:44:59 -0500

    added files

---
+++ liri.js
@@ -0,0 +1,127 @@
+// required libraries
+require("dotenv").config();
+var fs = require("fs");
+var moment = require('moment');
+var axios = require("axios");
+const keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var inquirer = require("inquirer");
+let spotify = new Spotify(keys.spotify);
+
+//pull in required variables
+var Spotify = process.env.SPOTIFY_ID
+let command = process.argv[2]
+let searchTerm = process.argv[3]
+
+fs.appendFile('log.txt', command + ",", function (err) {
+    if (err) throw err;
+});
+
+switch (command){
+    case "concert-this":  //bands in town
+        searchForBandsInTown(searchTerm);
+        break;
+    case "spotify-this-song":  //spotify
+        spotifyThisSong(searchTerm);
+        break;
+    case "movie-this":  // OMDB for movies
+        movieThis(searchTerm);
+        break;
+    case "do-what-it-says":  //  read commands from a file and excute the commands above
+        doRandom();
+    break;
+}
+    
+    function searchForBandsInTown(artist) {
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        axios.get(queryUrl).then(
+            function(response) {
+                if(response.data[0].venue !=  undefined) {
+                    console.log("Event Veunue: " + response.data[0].venue.name);
+                    console.log("Event Location: " + response.data[0].venue.city);
+                    var eventDateTime = moment(response.data[0].datetime);
+                    console.log("Event Date & Time: " + eventDateTime.format("dddd, MMMM Do YYYY"));
+                }
+                else {
+                    console.log("No results found.");
+                }
+            }
+        ).catch(function (error) {
+            console.log (error);
+      });
+    }
+
+    function spotifyThisSong(song) {
+        spotify
+        .search({ type: 'track', query: song })
+        .then(function(response){
+            if (response.tracks.total === 0) {
+                errorConditionForSpotify();
+            } else {
+                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Track: " + response.tracks.items[0].name);
+                console.log("Preview URL: " + response.tracks.items[0].preview_url);
+                console.log("Album: " + response.tracks.items[0].album.name);
+            }
+        }).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. Showing results for 'Fill Me In' by Craig David");
+      });
+    }
+
+    function errorConditionForSpotify() {
+        spotify
+        .search({ type: 'track', query: 'Fill Me In' })
+        .then(function(response) {
+            for (var i=0;i < response.tracks.items.length; i++) {
+                if (response.tracks.items[i].artists[0].name === "Craig David") {
+                    console.log("Artist: " + response.tracks.items[i].artists[0].name);
+                    console.log("Track: " + response.tracks.items[i].name);
+                    console.log("Preview URL: " + response.tracks.items[i].preview_url);
+                    console.log("Album: " + response.tracks.items[i].album.name);
+                    i = response.tracks.items.length;
+                }
+            }
+        }).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. ");
+      });
+    }
+
+    function movieThis(movie) {
+        axios.get(`http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`).then(
+            function(response) {
+                //console.log(response.data);
+                if (response.data.Title != undefined) {
+                    console.log("Title: " + response.data.Title);
+                    console.log("Year: " + response.data.Year);
+                    console.log("imdbRating:: " + response.data.imdbRating);
+                    console.log("Title: " + response.data.Title);
+                    console.log("Country:: " + response.data.Country);
+                    console.log("Language:: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                    console.log("RottenTomatoes: " + response.data.tomatoRating);
+                } 
+                else {
+                    movieThis("Little Shop of Horrors");
+                }
+            }
+            // if response is empty call the api again with the "default" movie 
+        ).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. ");
+      });
+    }
+
+    function doRandom() {
+        fs.readFile("random.txt", "utf8", function(error, data) {
+            var dataArr = data.split(",");
+            spotifyThisSong(dataArr[1])
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+              return console.log(error);
+            }
+        });
+    }
